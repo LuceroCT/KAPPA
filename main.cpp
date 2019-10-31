8be5611084da4e03dddea8f3e04f384702c05708
morphisjustfun <57011629+morphisjustfun@users.noreply.github.com>
2019-10-31 10:28:12 -0500

    Anadir codigo fuente
    El script "yape.sh" en bash linkea todos los 3 archivos para que generen un mismo output.

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include "clasesyfunciones.h"
+
+using namespace std;
+
+int main() {
+  vector<robot> robots;
+  int n;
+  int nrobots;
+  int roboeleccion;
+  cout << "Para disenar su almacen, inserte el numero de puntos" << endl;
+  cin >> n;
+  almacen fin(n);
+  cout << "El diseno de su almacen es el siguiente:" << endl;
+  imprimirmatriz(fin.getmatriz());
+  cout << "Cuantos robots desea en el almacen (de 1 a 3)" << endl;
+  cin >> nrobots;
+  cout << "Desea que la ubicacion de los robots se de forma aleatoria(1) o manual(2)" << endl;
+  cin >> roboeleccion;
+  if (roboeleccion == 1){
+    robotrandom(fin, nrobots);
+  }
+  else if (roboeleccion == 2){
+
+  }
+  cout << "Los robots estan dispuestos de la siguiente forma: " << endl;
+  imprimirmatriz(fin.getmatriz());
+}
+
+
+
+//https://github.com/LuceroCT/KAPPA/invitations}
---
+++ yape.sh
@@ -0,0 +1,4 @@
+g++ -o a main.cpp clasesyfunciones.cpp clasesyfunciones.h
+echo 
+./a
+rm a
